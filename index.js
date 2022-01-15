472292c646652a15659266f5f7395aa0436b762e
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2022-01-14 16:02:27 -0800

    Move events and commands into submodules

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-const { Client, Collection, Intents } = require('discord.js');
+const fs = require("fs");
+const { Client, Intents } = require("discord.js");
 
-const { token } = require('./config.json'); // process.env.DISCORD_TOKEN;
+const { token } = require("./config.json"); // process.env.DISCORD_TOKEN;
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-client.once('ready', () => {
-	console.log('Ready!');
-});
 
-client.commands = new Collection();
-const command = require(`./commands/wordle.js`);
-client.commands.set(command.data.name, command);
+const { commands } = require("./lib/commands.js");
+client.commands = commands;
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+const eventFiles = fs
+  .readdirSync("./events")
+  .filter((file) => file.endsWith(".js"));
 
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args));
+  }
+}
 client.login(token);
