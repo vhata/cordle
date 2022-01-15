14ddd0ebb454c5f580564883c6a020d1f111e187
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2022-01-14 17:52:22 -0800

    Add wordle word list and calculation

--- commands/wordle.js
+++ commands/wordle.js
@@ -1,10 +1,17 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { get_word } = require("../lib/words");
+
+const players = {};
+const answer = get_word(new Date());
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('wordle')
-		.setDescription('Plays a game of Wordle'),
-	async execute(interaction) {
-		await interaction.reply('Wordle!');
-	},
+  data: new SlashCommandBuilder()
+    .setName("wordle")
+    .setDescription("Plays a game of Wordle")
+    .addStringOption((option) =>
+      option.setName("word").setDescription("The word to guess")
+    ),
+  async execute(interaction) {
+    await interaction.reply({ content: "Wordle!", ephemeral: true });
+  },
 };
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,11 @@
+const { get_word } = require("../lib/words");
+
 module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+  name: "ready",
+  once: true,
+  execute(client) {
+    console.log(`Ready! Logged in as ${client.user.tag}`);
+    d = new Date();
+    console.log(`The word for ${d} is ${get_word(d)}`);
+  },
 };
