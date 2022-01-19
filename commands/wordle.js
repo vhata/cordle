90a825b5baafd708d4fc11a150453d53d74f9eb1
Jonathan Hitchcock <jonathan.hitchcock@gmail.com>
2022-01-18 16:53:02 -0800

    Working one-play wordle implementation

--- commands/wordle.js
+++ commands/wordle.js
@@ -1,8 +1,33 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { get_word } = require("../lib/words");
+const { day_number, get_word, valid_guess } = require("../lib/words");
 
 const players = {};
 const answer = get_word(new Date());
+
+const WRONG = 0;
+const MOVED = 1;
+const CORRECT = 2;
+
+const markers = ["⬛", "🟨", "🟩"];
+
+function try_guess(guess) {
+  guess = guess.substring(0, 5).toLowerCase();
+  results = Array(5).fill(WRONG);
+  a_copy = answer.toLowerCase().split("");
+  for (i = 0; i < 5; i++) {
+    if (a_copy[i] == guess[i]) {
+      results[i] = CORRECT;
+      a_copy[i] = "X";
+    }
+  }
+  for (i = 0; i < 5; i++) {
+    if (a_copy.includes(guess[i])) {
+      results[i] = MOVED;
+      a_copy[a_copy.findIndex((a) => a == guess[i])] = "X";
+    }
+  }
+  return results;
+}
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -12,6 +37,19 @@ module.exports = {
       option.setName("word").setDescription("The word to guess")
     ),
   async execute(interaction) {
-    await interaction.reply({ content: "Wordle!", ephemeral: true });
+    guess = interaction.options.getString("word");
+    if (!valid_guess(guess)) {
+      await interaction.reply({
+        content: "Not a valid word: " + guess,
+        ephemeral: true,
+      });
+    } else {
+      results = try_guess(interaction.options.getString("word"));
+      repl = "Wordle " + day_number(new Date()) + "\n";
+      for (r of results) {
+        repl = repl + markers[r];
+      }
+      await interaction.reply({ content: repl, ephemeral: false });
+    }
   },
 };
